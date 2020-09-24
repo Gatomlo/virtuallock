87de1b82fe151af81d258e28bece38e403f35d00
Thomas Verniers <thomas.verniers@gmail.com>
2020-09-24 13:54:49 +0200

    correction bug switch + ajout bouton enter sur login et password

--- public/js/createLock.js
+++ public/js/createLock.js
@@ -21,12 +21,12 @@ $('#document').ready(function(){
         });
         trying = arraySolution;
       break;
-      case 'diagram':
+    case 'diagram':
         var actualPattern = arraySolution.join('');
         diagram.setPattern(actualPattern);
         trying = arraySolution;
         break;
-      case 'directional':
+    case 'directional':
         $('#actualCode').html("");
         $.each(arraySolution, function(index,value){
           trying = arraySolution;
@@ -109,7 +109,7 @@ $('#document').ready(function(){
       trying = arraySolution;
         break;
       case 'switch':
-        var solution = []
+        var solution = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
         $.each(arraySolution, function(index,bool){
           if(bool == "true"){
             solution[index]=true
@@ -199,12 +199,13 @@ function addInterface(){
         '<div class="row justify-content-center"><div class="col oneDigit"><button button id="resetSwitch" type="button" class="btn digit btn-primary"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bootstrap-reboot" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 0 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8zm5.48-.079V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6zm0 3.75V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141z"/></svg></button></div></div> </div>'
         var solution = $('#lock_solution').val();
         var arraySolution = solution.split(',');
-        if (arraySolution == [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false] ){
+        if (solution == ''){
           trying = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
         }
         else{
           trying = arraySolution;
         }
+        console.log(trying)
         break;
   }
   $('#codeZone').html(codeInstance);
